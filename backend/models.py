456db9d1c09e98d5e00ff6b972120419f4f960ec
korotkov <durm@linux.site>
2015-04-10 15:27:15 +0300

    first

---
+++ backend/models.py
@@ -0,0 +1,37 @@
+#-*- coding: utf-8 -*-
+
+from sqlalchemy import Column, DateTime, String, Text, Integer, ForeignKey, func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Log(Base):
+    __tablename__ = "log"
+    
+    id = Column(Integer, primary_key=True)
+    host = Column(String)
+    event = Column(String)
+    event_desc = Column(Text)
+    obj = Column(String)
+    obj_desc = Column(Text)
+    subj = Column(String)
+    subj_desc = Column(Text)
+    result = Column(String)
+    result_desc = Column(Text)
+    timestamp = Column(DateTime, default=func.now())
+    
+    def __repr__(self):
+        return """
+        <log id="{0}">
+            <host>{1}</host>
+            <event>{2}</event>
+            <event_desc>{3}</event_desc>
+            <obj>{4}</obj>
+            <obj_desc>{5}</obj_desc>
+            <subj>{6}</subj>
+            <subj_desc>{7}</subj_desc>
+            <result>{8}</result>
+            <result_desc>{9}</result_desc>
+            <timestamp>{10}</timestamp>
+        </log>
+        """.format(str(self.id), self.host, self.event, self.event_desc, self.obj, self.obj_desc, self.subj, self.subj_desc, self.result, self.result_desc, str(self.timestamp.isoformat()))
---
+++ monitor/start.py
@@ -0,0 +1,65 @@
+#-*- coding: utf-8 -*-
+
+from flask import Flask, send_file, abort, url_for, request, render_template, redirect, Response
+from beholder.backend.engine import session as dbsession
+from beholder.backend.models import Log
+from datetime import datetime
+
+app = Flask(__name__)
+
+DEFAULTSIZE = 20
+MAXSIZE = 100
+DEFAULTSTART = 0
+LOGPARAMS = {
+    "eq": ("id", "host", "event", "obj", "subj", "result",),
+    "contains": ("event_desc", "obj_desc", "subj_desc", "result_desc",),
+    "gte": ("timestamp_from",),
+    "lte": ("timestamp_to",)
+}
+
+def datetime_from_iso(sdt):
+    return datetime.strptime(sdt, "%Y-%m-%dT%H:%M:%S")   
+    
+def filter_items(items, args):
+    for k, params in LOGPARAMS.items() :
+        for param in params :
+            if param in args :
+                if k == "eq" :
+                    items = items.filter(getattr(Log, param) == args[param])
+                elif k == "contains" :
+                    items = items.filter(getattr(Log, param).contains(args[param]))
+                elif k == "gte" and param.endswith("_from") :
+                    p = param.split("_from")[0]
+                    items = items.filter(getattr(Log, p) >= datetime_from_iso(args[param]))
+                elif k == "lte" and param.endswith("_to") :
+                    p = param.split("_to")[0]
+                    items = items.filter(getattr(Log, p) <= datetime_from_iso(args[param]))
+                else:
+                    pass
+    return items
+
+@app.route("/logs.xml")
+def logs_xml():
+
+    size = int(request.args.get("size", DEFAULTSIZE))
+    size = size if size <= MAXSIZE else MAXSIZE
+    
+    start = int(request.args.get("start", DEFAULTSTART))
+    
+    finish = start + size
+    
+    s = dbsession()
+    
+    items = s.query(Log)    
+    items = filter_items(items, request.args)
+    items = items[start:finish]
+    
+    body = """<logs start="{0}" size="{1}">{2}</logs>""".format(str(start), str(size), "".join(map(repr, items)))
+    
+    s.close()
+    
+    return Response(body, mimetype="text/xml")
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
